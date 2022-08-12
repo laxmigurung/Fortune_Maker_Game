3b315203ce3bed4c2ec2e4cf5c48bc7db5cb91e7
Laxmi Gurung <laxmigrg167@gmail.com>
2022-08-11 23:02:26 -0400

    modified game

--- views/game.py
+++
@@ -1,7 +0,0 @@
-class Game:
-    def __init__(self, attempts):
-        self.attempts = 10
-
-    def attempts_track(self):
-        self.attempts = self.attempts - 1
-        return self.attempts
--- views/message.py
+++ views/message.py
@@ -1,10 +1,13 @@
-from database.insert_table import insert_player, player_guess
+from typing import List
+
+from database.insert_table import insert_player
 from views.game_level import Game
 from views.player_information import get_player_info
 
-player = get_player_info()
+
+'''player = get_player_info()
 insert_player(player[0], player[1])
-player_name = player[0]
+player_name = player[0]'''
 
 
 def start_game():
